5fbae2049896b105ce8facc83602953c5ae17f6a
Jon Wallace <jonmon6691@gmail.com>
2024-12-01 15:00:26 -0800

    Use regex instead of split_whitespace
    Note sure this is better in any way though...

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,40 +1,43 @@
 use std::path::Path;
 
-pub fn do_d01_1() -> i32 {
-    let input_path = Path::new("input/input01-1.txt");
-    let (mut team_a, mut team_b) =
-        crate::load_input_2_cols(&input_path).expect("Error loading input");
+pub fn do_d01_1() -> Result<u32, String> {
+    let (mut team_a, mut team_b) = crate::load_input_2_cols(&Path::new("input/input01-1.txt"))?;
+
     team_a.sort();
     team_b.sort();
-    team_a
+
+    Ok(team_a
         .into_iter()
         .zip(team_b.into_iter())
-        .map(|(a, b)| -> i32 { (a - b).abs() })
-        .sum()
+        .map(|(a, b)| a.abs_diff(b))
+        .sum())
 }
 
-pub fn do_d01_2() -> i32 {
-    let input_path = Path::new("input/input01-1.txt");
-    let (team_a, team_b) =
-        crate::load_input_2_cols(&input_path).expect("Error loading input");
+pub fn do_d01_2() -> Result<i32, String> {
+    let (team_a, team_b) = crate::load_input_2_cols(&Path::new("input/input01-1.txt"))?;
+
     let a_counts = crate::get_counts(&team_a);
     let b_counts = crate::get_counts(&team_b);
-    a_counts.iter().map(|(a_val, a_count)| {
-        let b_count: i32 = b_counts.get(a_val).cloned().unwrap_or(0) as i32;
-        a_val * (*a_count as i32) * b_count
-    }).sum()
+
+    Ok(a_counts
+        .iter()
+        .map(|(a_val, a_count)| {
+            let b_count: i32 = b_counts.get(a_val).cloned().unwrap_or(0) as i32;
+            a_val * (*a_count as i32) * b_count
+        })
+        .sum())
 }
 
 #[test]
 fn test_day_01_1() {
     let ans = do_d01_1();
-    let day01_01_obfuscated_answer = 0x55555555 ^ ans;
-    assert_eq!(dbg!(day01_01_obfuscated_answer), 1432673504);
+    let day01_01_obfuscated_answer = ans.map(|answer| dbg!(answer) ^ 0x55555555);
+    assert_eq!(day01_01_obfuscated_answer, Ok(1432673504));
 }
 
 #[test]
 fn test_day_01_2() {
     let ans = do_d01_2();
-    let day01_02_obfuscated_answer = 0x55555555 ^ ans;
-    assert_eq!(dbg!(day01_02_obfuscated_answer), 1419059134);
+    let day01_02_obfuscated_answer = ans.map(|answer| dbg!(answer) ^ 0x55555555);
+    assert_eq!(day01_02_obfuscated_answer, Ok(1419059134));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,44 @@
 pub mod day_01;
 
-use itertools::Itertools;
 use std::collections::HashMap;
 use std::io::{self, BufRead};
 use std::{fs::File, path::Path};
+use regex::Regex;
 
-pub fn load_input_2_cols(input_file: &Path) -> Result<(Vec<i32>, Vec<i32>), ()> {
-    let lines = read_lines(input_file).map_err(|err| {
-        eprintln!("Error reading file {input_file:?} ({err})");
-    })?;
+pub fn load_input_2_cols(input_file: &Path) -> Result<(Vec<i32>, Vec<i32>), String> {
+    let lines = read_lines(input_file)
+        .map_err(|err| format!("Error reading file {input_file:?} ({err})"))?;
+
+    // Matches two integers separated by whitespace
+    let line_parser = Regex::new(r"^\s*(\d+)\s+(\d+)\s*$").unwrap();
+
     Ok(lines
-        .flatten()
-        .map(|line: String| {
-            row_to_2_ints(&line)
-        })
+        .flatten() // Handles the Result<line, _> TODO: What happens to the error?
+        .enumerate() // For line numbers in the error report
+        .map(|(line_num, line)| {
+            match line_parser.captures(&line) {
+                Some(matches) => {
+                    // These unwraps can't fail because of how the regex is written, errors will all fall into the None match arm below
+                    let left = matches.get(1).unwrap().as_str().parse().unwrap();
+                    let right = matches.get(2).unwrap().as_str().parse().unwrap();
+                    Ok((left, right))
+                },
+                None => {
+                    let line_num = line_num + 1;
+                    Err(format!("Input Error: Line {line_num} in {input_file:?}: Expected two whitespace-separated ints, but got \"{line}\""))
+                }}
+            })
         // I wish I knew how to do this without allocating storage :/
-        .collect::<Result<Vec<(i32, i32)>, ()>>()?
+        .collect::<Result<Vec<(i32, i32)>, String>>()?
         .into_iter()
         .unzip())
-}
-
-fn row_to_2_ints(row: &str) -> Result<(i32, i32), ()> {
-    row.split_whitespace()
-        .map(|cell| cell.parse().map_err(|err| {
-            eprintln!("Error parsing int from string \"{cell}\" ({err})")
-        }))
-        // I wish I knew how to do this without allocating storage :/
-        .collect::<Result<Vec<i32>, ()>>()?
-        .into_iter()
-        .tuples()
-        .next()
-        .ok_or_else(|| eprintln!("Error parsing columns from row \"{row}\", expecting 2 ints separated by whitespace"))
 }
 
 fn get_counts(data: &[i32]) -> HashMap<i32, isize> {
     let mut counts = HashMap::new();
     for num in data {
-        counts.entry(num.clone())
+        counts
+            .entry(num.clone())
             .and_modify(|v| *v += 1)
             .or_insert(1);
     }

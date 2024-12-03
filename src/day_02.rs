d297f0b987429f8720dabebd83e3e3194a94a970
Jon Wallace <jonmon6691@gmail.com>
2024-12-02 19:03:09 -0800

    Docs cleanup

--- src/day_02.rs
+++ src/day_02.rs
@@ -3,16 +3,17 @@ use std::{cmp::Ordering, path::Path};
 
 /// Checks the safety of a given reading
 fn check_reading_safety(reading: &[i32]) -> bool {
+    // Keeps track of an increasing or decreasing trend, None until the first pair is observed
     let mut direction: Option<Ordering> = None;
     reading
         .iter()
         .tuple_windows()
         .map(|(a, b)| {
-            direction = direction.or(Some(b.cmp(a)));
+            direction = direction.or(Some(b.cmp(a))); // .or is only taken on the first loop and sets direction to Some()thing
             match *b - *a {
                 -3..=-1 => direction == Some(Ordering::Less),
                 1..=3 => direction == Some(Ordering::Greater),
-                _ => false,
+                _ => false, // 0 or ABS() > 3
             }
         })
         .all(bool::into)
@@ -32,8 +33,8 @@ pub fn do_d02_2() -> Result<usize, String> {
         crate::load_input_vec_of_vecs(Path::new("input/input02.txt"))?
             .iter()
             .filter(|reading| {
-                check_reading_safety(reading)
-                    || (0..reading.len()) // Try removing one level at a time until the reading passes (Probably not optimal)
+                check_reading_safety(reading) // Check the nominal case (no deletions)
+                    || (0..reading.len()) // OR try removing one level at a time until the reading passes (Probably not optimal)
                         .map(|i| check_reading_safety(&crate::drop_i(reading, i)))
                         .any(bool::into)
             })
@@ -43,14 +44,12 @@ pub fn do_d02_2() -> Result<usize, String> {
 
 #[test]
 fn test_day_02_1() {
-    let ans = do_d02_1();
-    let day01_01_obfuscated_answer = ans.map(|answer| dbg!(answer) ^ 0x55555555);
-    assert_eq!(day01_01_obfuscated_answer, Ok(1431655823));
+    let obfuscated_answer = do_d02_1().map(|answer| dbg!(answer) ^ 0x55555555);
+    assert_eq!(obfuscated_answer, Ok(1431655823));
 }
 
 #[test]
 fn test_day_02_2() {
-    let ans = do_d02_2();
-    let day01_02_obfuscated_answer = ans.map(|answer| dbg!(answer) ^ 0x55555555);
-    assert_eq!(day01_02_obfuscated_answer, Ok(1431655543));
+    let obfuscated_answer = do_d02_2().map(|answer| dbg!(answer) ^ 0x55555555);
+    assert_eq!(obfuscated_answer, Ok(1431655543));
 }

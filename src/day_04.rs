d3b6810958266ae9607239e0f6dad8d57b56092d
Jon Wallace <jonmon6691@gmail.com>
2024-12-05 19:31:57 -0800

    Dont judge me, Im 2 days behind ok

--- src/day_04.rs
+++ src/day_04.rs
@@ -68,8 +68,29 @@ fn test_day_04_1() {
     assert_eq!(obfuscated_answer, Ok(1431658354));
 }
 
-pub fn do_d04_2() -> Result<i32, String> {
-    Err("TODO!".to_owned())
+pub fn do_d04_2() -> Result<usize, String> {
+    let og = &crate::load_input_utf8(Path::new("input/input_04.txt"))?;
+    let line_len = og.lines().next().unwrap().len() - 1;
+
+    let down =
+        Regex::new(&format!("^(?s)M.M.{{{line_len}}}A.{{{line_len}}}S.S")).expect("Invalid regex");
+    let up =
+        Regex::new(&format!("^(?s)S.S.{{{line_len}}}A.{{{line_len}}}M.M")).expect("Invalid regex");
+    let right =
+        Regex::new(&format!("^(?s)M.S.{{{line_len}}}A.{{{line_len}}}M.S")).expect("Invalid regex");
+    let left =
+        Regex::new(&format!("^(?s)S.M.{{{line_len}}}A.{{{line_len}}}S.M")).expect("Invalid regex");
+
+    let matches = (0..(og.len() - line_len * 2 + 7))
+        .map(|i| {
+            down.find_iter(&og[i..]).count()
+                + up.find_iter(&og[i..]).count()
+                + right.find_iter(&og[i..]).count()
+                + left.find_iter(&og[i..]).count()
+        })
+        .sum();
+
+    Ok(matches)
 }
 
 #[test]

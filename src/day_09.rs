d4ba506f7c0c6a56273dcfd721149e6bd4bd0ee7
Jon Wallace <jonmon6691@gmail.com>
2024-12-08 22:22:46 -0800

    D9-1 done

---
+++ src/day_09.rs
@@ -0,0 +1,91 @@
+use std::{
+    iter::{once, repeat_n},
+    path::Path,
+};
+
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct File {
+    id: usize,
+    size: usize,
+    offset: usize,
+}
+
+pub fn part_1() -> Result<usize, String> {
+    let raw = crate::load_input_utf8(Path::new("input/input_09.txt"))?;
+
+    let mut memory: Vec<Option<usize>> = raw
+        .chars()
+        .chain(once('0'))
+        .tuples()
+        .enumerate()
+        .map(|(i, (file, free))| {
+            repeat_n(Some(i), file.to_digit(10).unwrap() as usize)
+                .chain(repeat_n(None, free.to_digit(10).unwrap() as usize))
+        })
+        .flatten()
+        .collect();
+
+    let mut head = 0;
+    let mut tail = memory.len() - 1;
+    while head != tail {
+        // print_mem(&memory, head, tail);
+        println!();
+        if memory[head] != None {
+            head += 1;
+            continue;
+        }
+        if memory[tail] == None {
+            tail -= 1;
+            continue;
+        }
+        memory[head] = memory[tail];
+        head += 1;
+        tail -= 1;
+    }
+    Ok(memory[0..=head]
+        .iter()
+        .copied()
+        .while_some()
+        .enumerate()
+        .map(|(i, id)| i * id)
+        .sum())
+}
+
+fn print_mem(mem: &Vec<Option<usize>>, head: usize, tail: usize) {
+    for v in mem {
+        match v {
+            Some(id) => print!("{} ", id),
+            None => print!(". "),
+        }
+    }
+    println!();
+    for _ in 0..head {
+        print!("  ");
+    }
+    print!("H ");
+    for _ in head..tail - 1 {
+        print!("  ");
+    }
+    print!("T ({}, {})", head, tail);
+}
+
+#[test]
+fn test_part_1() {
+    let obfuscated_answer = part_1().map(|answer| dbg!(answer) ^ 0x55555555);
+    assert!(obfuscated_answer.is_ok());
+    assert_eq!(obfuscated_answer, obfuscated_answer);
+}
+
+pub fn part_2() -> Result<usize, String> {
+    let raw = crate::load_input_utf8(Path::new("input/input_06.txt"))?;
+    Ok(0)
+}
+
+#[test]
+fn test_part_2() {
+    let obfuscated_answer = part_2().map(|answer| dbg!(answer) ^ 0x55555555);
+    assert!(obfuscated_answer.is_ok());
+    assert_eq!(obfuscated_answer, obfuscated_answer);
+}
